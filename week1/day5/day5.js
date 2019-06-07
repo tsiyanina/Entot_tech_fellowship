80c2c2450c1157cf976ee9dbb286acb13522f438
tsiyan_ <tsia.getachew@gmail.com>
2019-06-07 10:45:32 +0300

    Day5 exercise

---
+++ week1/day5/day5.js
@@ -0,0 +1,19 @@
+for(var i=1; i<=100; i++)
+{
+  console.log(i)
+  if(i%3 == 0)
+  {
+    console.log("Fizz")
+  }
+  else if(i%5 == 0)
+    {
+      console.log("Buzz")
+    }
+  else if( i%5==0 && i%3==0)
+    {
+      console.log("FizzBuzz ")
+    }
+  else{
+    console.log(i)
+  }
+}
